b7814a5a84e53a7bad1273fbf35fd9c3be4f6b61
sanjai-lab-dev <sanjaydhesigan@gmail.com>
2026-05-25 10:59:25 +0530

    backend cors update next

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
-import cors from 'cors'
+
 import connectDB from './config/db'
 
 // Route imports
@@ -12,21 +12,39 @@ import userRoutes from './routes/userRoutes'
 
 const app = express()
 
-// CORS CONFIG
-const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:3000',
+
+
+// Manual CORS Middleware
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
     'https://smart-leads-manager-u3md.vercel.app'
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}
+  )
 
-// Middleware
-app.use(cors(corsOptions))
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
 
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  )
+
+  res.header(
+    'Access-Control-Allow-Credentials',
+    'true'
+  )
+
+  console.log(req.method, req.url)
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
 
 
 app.use(express.json())
